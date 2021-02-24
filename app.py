564b05fca2b1e27098ae4bb09930e45e02a8a8c4
taeheelee90 <taehee.lee90@gmail.com>
2021-02-24 18:16:34 +0200

    Change Stock API provider

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 from flask import Flask, render_template
-import requests
 import weather 
 import stock
-
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # GET Porvoo Weather   
+    # GET Porvoo Weather Information
     porvoo_result = weather.get_weather_info('porvoo')
     # GET Seoul Weather
     seoul_result = weather.get_weather_info('seoul')
 
-    # Stock information
-    company_code = {'samsung':'005930.KS', 'kakao':'035720.KS'}    
-    samsung = stock.get_stock_info(company_code['samsung'])
-    kakao = stock.get_stock_info(company_code['kakao'])
+    # Get Stock Information   
+    samsung_result = stock.get_stock_info('https://finance.yahoo.com/quote/005930.KS?p=005930.KS&.tsrc=fin-srch')
+    kakao_result = stock.get_stock_info('https://finance.yahoo.com/quote/035720.KS?p=035720.KS&.tsrc=fin-srch')
+    apple_result = stock.get_stock_info('https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch')
+    tesla_result = stock.get_stock_info('https://finance.yahoo.com/quote/TSLA?p=TSLA&.tsrc=fin-srch')
+    neste_result = stock.get_stock_info('https://finance.yahoo.com/quote/NESTE.HE?p=NESTE.HE&.tsrc=fin-srch')
+    stocks=[{'samsung' : samsung_result, 'kakao' : kakao_result, 'apple' : apple_result, 'tesla' : tesla_result, 'neste' : neste_result}]
+
 
     return render_template('main.html', 
                             porvoo = porvoo_result,  
-                            seoul = seoul_result, 
-                            samsung = samsung.to_html(),
-                            kakao = kakao.to_html())
+                            seoul = seoul_result,
+                            stocks = stocks
+                            )
 
 if __name__ == '__main__':
     app.run()
--- stock.py
+++ stock.py
@@ -1,7 +1,12 @@
-import yfinance as yf
-from datetime import datetime
+from bs4 import BeautifulSoup
+import requests
 
-def get_stock_info(company):
-    today = datetime.today().strftime('%Y-%m-%d')
-    return yf.download(company, start = today)
+def get_stock_info(search_url):
+
+    req = requests.get(search_url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    # Get Stock Information
+    now = soup.find('span', attrs={'class': 'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text.replace('.00', '')
+    return now
     
